8a72f7bf696e7aa5bbca556fbf966575aa380352
devforfu <developer.z@outlook.com>
2018-04-30 19:53:47 +0500

    Logistic classifier and base class signatures refactoring

---
+++ right_whale_recognition/utils.py
@@ -0,0 +1,103 @@
+import re
+from collections import OrderedDict
+
+import numpy as np
+import tensorflow as tf
+
+
+class ArrayBatchGenerator:
+    def __init__(self, *arrays, same_size_batches=False, batch_size=32,
+                 infinite=False, shuffle_on_restart=False):
+
+        assert same_length(arrays)
+        self.same_size_batches = same_size_batches
+        self.batch_size = batch_size
+        self.infinite = infinite
+        self.shuffle_on_restart = shuffle_on_restart
+
+        total = len(arrays[0])
+        n_batches = total // batch_size
+        if same_size_batches and (total % batch_size != 0):
+            n_batches += 1
+
+        self.array_size = len(arrays[0])
+        self.current_batch = 0
+        self.n_batches = n_batches
+        self.arrays = list(arrays)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self.current_batch == self.n_batches:
+            if not self.infinite:
+                raise StopIteration()
+            self.current_batch = 0
+            if self.shuffle_on_restart:
+                index = np.random.permutation(self.array_size)
+                self.arrays = [arr[index] for arr in self.arrays]
+
+        start = self.current_batch * self.batch_size
+        batches = [arr[start:(start + self.batch_size)] for arr in self.arrays]
+        self.current_batch += 1
+        return batches
+
+
+def same_length(*arrays):
+    first, *rest = arrays
+    n = len(first)
+    for arr in rest:
+        if len(arr) != n:
+            return False
+    return True
+
+
+def add_to_collection(name, *ops):
+    """Combines group of tensors into collection."""
+    for op in ops:
+        tf.add_to_collection(name, op)
+
+
+def get_collection(name):
+    ops = OrderedDict()
+    for op in tf.get_collection(name):
+        ops[short_name(name, op.name)] = op
+    if len(ops) == 1:
+        return list(ops.values())[0]
+    return ops
+
+
+def short_name(collection_name, tensor_name):
+    match = re.match(f'^{collection_name}/(\w+)(:\d)?', tensor_name)
+    name, *_ = match.groups()
+    return name
+
+
+def as_string(dictionary, stats_formats=None):
+    if stats_formats is None:
+        stats_formats = OrderedDict()
+        stats_formats['epoch'] = '05d'
+        stats_formats['loss'] = '2.6f'
+        stats_formats['val_loss'] = '2.6f'
+        stats_formats['accuracy'] = '2.2%'
+        stats_formats['val_accuracy'] = '2.2%'
+
+    format_strings = [
+        '%s: {%s:%s}' % (name, name, value)
+        for name, value in stats_formats.items()]
+    format_string = ' - '.join(format_strings)
+    return format_string.format(**dictionary)
+
+
+def cross_entropy(num_classes, logits, labels, onehot=True):
+    assert num_classes >= 2, 'Number of classes cannot be less then 2'
+    if num_classes == 2:
+        func = tf.nn.sigmoid_cross_entropy_with_logits
+    elif onehot:
+        func = tf.nn.softmax_cross_entropy_with_logits
+    else:
+        func = tf.nn.sparse_softmax_cross_entropy_with_logits
+    return func(logits=logits, labels=labels)
